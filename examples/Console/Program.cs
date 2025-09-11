f8f47c2700cf25bcf661be6614677095e35b831e
Jeffrey (OnSpec) <231130936+jeffrey-onspec@users.noreply.github.com>
2025-09-11 14:18:24 -0500

    feat(console-example): adds console example
    Creates a console example project to demonstrate the usage of the HostBridge library.
    
    Includes configuration, logging, and hosted service setup for a basic application.
    Also adds binding redirects for .NET Framework.

--- examples/Console/Program.cs
+++ examples/Console/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Console
+using HostBridge.Core;
+using HostBridge.Options.Config;
+
+using JetBrains.Annotations;
+
+using Microsoft.Extensions.Logging;
+
+namespace Examples.Console;
+
+[UsedImplicitly]
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        var host = new LegacyHostBuilder()
+            .ConfigureAppConfiguration(cfg => cfg.AddHostBridgeAppConfig())
+            .ConfigureLogging(lb => lb.AddConsole())
+            .ConfigureServices((_, services) =>
+            {
+                services.AddHostedService<HeartbeatService>();
+            })
+            .Build();
+
+        using var runFor30Seconds = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+        await host.RunAsync(runFor30Seconds.Token, shutdownTimeout: TimeSpan.FromSeconds(10));
     }
 }
